262021e11ca7a292019c71bf5b42670bef3b91f5
@aamutezar <aamutezar@gmail.com>
2023-03-13 16:47:02 +0700

    update templating blade index & explore

--- app/Http/Controllers/Landing/LandingController.php
+++ app/Http/Controllers/Landing/LandingController.php
@@ -3,16 +3,23 @@
 namespace App\Http\Controllers\Landing;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class LandingController extends Controller
 {
+
+    // public function __invoke()
+    // {
+        
+    // }
+
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): View
     {
-        //
+        return view('pages.landing.index');
     }
 
     /**
@@ -64,9 +71,9 @@ class LandingController extends Controller
     }
 
     
-    public function explore()
+    public function explore(): View
     {
-
+        return view('pages.landing.explore');
     }
 
     public function detail($id)
--- routes/web.php
+++ routes/web.php
@@ -39,25 +39,25 @@ use Symfony\Component\Routing\RequestContext;
 // });
 
 
-Route::get('detail_booking/{id}', LandingController::class, 'detail_booking')->name('detail.booking.landing');
-Route::get('booking/{id}', LandingController::class, 'booking')->name('booking.landing');
-Route::get('detail/{id}', LandingController::class, 'detail')->name('detail.landing');
-Route::get('explore', LandingController::class, 'explore')->name('explore.landing');
-Route::get('/', LandingController::class);
+Route::get('/', [LandingController::class, 'index'])->name('index.landing');
+Route::get('detail_booking/{id}', [LandingController::class, 'detail_booking'])->name('detail.booking.landing');
+Route::get('booking/{id}', [LandingController::class, 'booking'])->name('booking.landing');
+Route::get('detail/{id}', [LandingController::class, 'detail'])->name('detail.landing');
+Route::get('explore', [LandingController::class, 'explore'])->name('explore.landing');
 
 Route::name('member.')->middleware(['auth:sanctum', 'verified'])->group(function(){
 
     Route::resource('dashboard', MemberController::class);
     Route::resource('service', ServiceController::class);
 
-    Route::get('approve_request/{id}', RequestController::class, 'approve')->name('approve.request');
+    Route::get('approve_request/{id}', [RequestController::class, 'approve'])->name('approve.request');
     Route::resource('request', RequestController::class);
 
-    Route::get('accept/order/{id}', MyOrderController::class, 'accepted')->name('accept.order');
-    Route::get('reject/order/{id}', MyOrderController::class, 'rejected')->name('reject.order');
+    Route::get('accept/order/{id}', [MyOrderController::class, 'accepted'])->name('accept.order');
+    Route::get('reject/order/{id}', [MyOrderController::class, 'rejected'])->name('reject.order');
     Route::resource('order', MyOrderController::class);
 
-    Route::get('delete_photo', ProfileController::class, 'delete')->name('delete.photo.profile');
+    Route::get('delete_photo', [ProfileController::class, 'delete'])->name('delete.photo.profile');
     Route::resource('profile', ProfileController::class);
 
 });
